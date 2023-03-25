629b77ba01e9aee8339c78c5ec965882182152ea
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-03-25 11:38:26 -0300

    Create static_final_const.dart

---
+++ static_final_const.dart
@@ -0,0 +1,20 @@
+class Valores {
+  static const tamanhoBotoes = 20.0;
+  static int vezesClicado = 0;
+  static void teste() {
+  print("Teste!");
+  }
+}
+class Pessoa {
+
+}
+void main() {
+  Valores.vezesClicado = 2;
+  print(Valores.vezesClicado);
+
+  Valores.teste();
+  print(Valores.tamanhoBotoes);
+  //Valores.tamanhoBotoes = 10;
+  final Pessoa pessoa = Pessoa();
+  final pessoa2 = Pessoa();
+}
