19582f11ad1998d3514b0af5b35f39646145897b
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-03-25 11:43:40 -0300

    Create abstrata.dart

---
+++ abstrata.dart
@@ -0,0 +1,58 @@
+abstract class Animal {
+  String nome;
+  double peso;
+  Animal(this.nome, this.peso);
+  void comer() {
+    print("$nome comeu!");
+  }
+  void fazerSom();
+  @override
+  String toString() {
+    return "Animal | Nome: $nome, Peso: $peso";
+  }
+
+}
+class Cachorro extends Animal {
+  int habilidade;
+  Cachorro(String nome, double peso, this.habilidade) : super(nome,peso);
+  void brincar() {
+    habilidade += 10;
+    print("habilidade do $nome aumentou para $habilidade!!!");
+  }
+  @override
+  void fazerSom() {
+    print("$nome fez au au!");
+  }
+  @override
+  String toString() {
+    return "Cachorro | Nome: $nome, Peso: $peso, habilidade: $habilidade";}
+}
+class Gato extends Animal {
+  Gato(String nome, double peso) : super(nome, peso);
+  bool estaAmigavel() {
+    return true;
+  }
+  @override
+  void fazerSom() {
+    print("$nome fez miaaauuu!");
+  }
+  @override
+  String toString() {
+    return "Gato | Nome: $nome, Peso: $peso";
+  }
+}
+void main() {
+  //Animal animal = Animal("Vai daa ERRo",10.0);
+  Cachorro cachorro = Cachorro("Dog", 10.0, 100);
+  cachorro.fazerSom();
+  cachorro.comer();
+  cachorro.brincar();
+  print(cachorro);
+  
+  Gato gato = Gato("Cat", 10.0);
+  gato.fazerSom();
+  gato.comer();
+  print("Esta amigável? ${gato.estaAmigavel()}");
+  print(gato);
+
+}
