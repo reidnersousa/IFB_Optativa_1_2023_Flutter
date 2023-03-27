f597c188dc9f0e7c2fff594461be6fee6a040366
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-03-27 17:15:52 -0300

    Create Ex2.dart

---
+++ Ex2.dart
@@ -0,0 +1,15 @@
+//Ex 2
+
+double produto(double valor1, double valor2){
+  return valor1*valor2;
+}
+
+
+void produtoImprimir(double valor1, double valor2){
+  print(valor1*valor2);
+}
+
+void main() {
+  print(produto(10,20));
+  produtoImprimir(10,20);
+}
