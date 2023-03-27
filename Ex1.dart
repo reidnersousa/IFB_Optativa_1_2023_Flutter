8cfbc1ccc2b09d166d2e13b207841418cdd245b3
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-03-27 17:42:03 -0300

    Update Ex1.dart

--- Ex1.dart
+++ Ex1.dart
@@ -21,27 +21,4 @@ void main() {
   Carro car1= Carro("KJM10","Azul",2010);
   print(car1);
   
-}// Ex1 
 
-class Carro{
-  String placa="";
-  String cor=""; 
-  int ano=0;
-  Carro(String placa,String cor , int ano){
-    this.placa = placa;
-    this.cor   = cor;
-    this.ano   = ano;
-  }
-  toString(){
-    return "Placa $placa Cor: $cor Ano: $ano";
-  }
-}
-
-
-
-
-void main() {
-  Carro car1= Carro("KJM10","Azul",2010);
-  print(car1);
-  
-}
