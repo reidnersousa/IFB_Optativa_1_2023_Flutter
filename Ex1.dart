7c39a0afc9c7251b387ee6a4de35fd6ff66fd93a
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-03-27 17:09:55 -0300

    Create Ex1.dart

---
+++ Ex1.dart
@@ -0,0 +1,47 @@
+// Ex1 
+
+class Carro{
+  String placa="";
+  String cor=""; 
+  int ano=0;
+  Carro(String placa,String cor , int ano){
+    this.placa = placa;
+    this.cor   = cor;
+    this.ano   = ano;
+  }
+  toString(){
+    return "Placa $placa Cor: $cor Ano: $ano";
+  }
+}
+
+
+
+
+void main() {
+  Carro car1= Carro("KJM10","Azul",2010);
+  print(car1);
+  
+}// Ex1 
+
+class Carro{
+  String placa="";
+  String cor=""; 
+  int ano=0;
+  Carro(String placa,String cor , int ano){
+    this.placa = placa;
+    this.cor   = cor;
+    this.ano   = ano;
+  }
+  toString(){
+    return "Placa $placa Cor: $cor Ano: $ano";
+  }
+}
+
+
+
+
+void main() {
+  Carro car1= Carro("KJM10","Azul",2010);
+  print(car1);
+  
+}
