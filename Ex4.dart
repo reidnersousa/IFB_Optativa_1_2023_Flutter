995a40181755bad841e5477fc323388d5ab58cfa
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-03-27 17:59:48 -0300

    Create Ex4.dart

---
+++ Ex4.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (int i = 1; i < 101; i++) {
+    if(i % 2==0){
+      print("Valor de i=${i} ==> Par");
+    }
+    else if(i % 2 !=0){
+      print("Valor de i=$i ==> Impar");
+    }
+  }
+}
