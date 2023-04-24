6c532969634c5deedd69df0f97243fcef9c63430
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2023-04-24 17:00:22 -0300

    Update futureAwait.dart

--- futureAwait.dart
+++ futureAwait.dart
@@ -7,4 +7,15 @@ Future<int> buscarValor() async {
 void main() async {
   int valor = await buscarValor();
   print(valor); // imprime 42
+  buscarDados().then((dados) {
+  print(dados); // imprime "Dados retornados após 2 segundos"
+    });
+}
+
+
+
+Future<String> buscarDados() {
+  return Future.delayed(Duration(seconds: 2), () {
+    return "Dados retornados após 2 segundos";
+  });
 }
